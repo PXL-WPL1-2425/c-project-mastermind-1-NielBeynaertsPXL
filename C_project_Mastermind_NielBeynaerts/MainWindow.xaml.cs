e8f02b2b3fa85813c23579d5785230af9124761c
NielBeynaertsPXL <niel.beynaerts@student.pxl.be>
2024-11-04 14:21:13 +0100

    Mastermind-01

---
+++ C_project_Mastermind_NielBeynaerts/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace C_project_Mastermind_NielBeynaerts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void color1Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void color2Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void color3Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void color4Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
