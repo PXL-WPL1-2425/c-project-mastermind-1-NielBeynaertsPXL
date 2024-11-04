3c46395eeca4ee9f33add3f3ccadd391c41fafc8
NielBeynaertsPXL <niel.beynaerts@student.pxl.be>
2024-11-04 16:29:00 +0100

    MasterMind-04

--- C_project_Mastermind_NielBeynaerts/MainWindow.xaml.cs
+++ C_project_Mastermind_NielBeynaerts/MainWindow.xaml.cs
@@ -1,22 +1,14 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Effects;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace C_project_Mastermind_NielBeynaerts
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Button selectedButton; // Track the currently selected button
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,36 +20,84 @@ namespace C_project_Mastermind_NielBeynaerts
             Random rnd = new Random();
             for (int i = 0; i < randomColors.Length; i++)
             {
-                randomColors[i] = rnd.Next(1,6);
+                randomColors[i] = rnd.Next(0, colors.Length); // Adjusted to use 0-based index
             }
-            this.Title = $"{colorsName[randomColors[0]].ToString()}, {colorsName[randomColors[1]].ToString()}, {colorsName[randomColors[2]].ToString()}, {colorsName[randomColors[3]].ToString()}";
+            this.Title = $"{colorsName[randomColors[0]]}, {colorsName[randomColors[1]]}, {colorsName[randomColors[2]]}, {colorsName[randomColors[3]]}";
         }
-
 
 
 
         private void color1Button_Click(object sender, RoutedEventArgs e)
         {
-
+            selectedButton = color1Button; // Set selected button to color1Button
+            radioButtonsGroupBox.Visibility = Visibility.Visible;
+            uncheckRadioButtons();
         }
 
         private void color2Button_Click(object sender, RoutedEventArgs e)
         {
+            selectedButton = color2Button; // Set selected button to color2Button
             radioButtonsGroupBox.Visibility = Visibility.Visible;
-
-
+            uncheckRadioButtons();
         }
 
         private void color3Button_Click(object sender, RoutedEventArgs e)
         {
+            selectedButton = color3Button; // Set selected button to color3Button
             radioButtonsGroupBox.Visibility = Visibility.Visible;
-
+            uncheckRadioButtons();
         }
 
         private void color4Button_Click(object sender, RoutedEventArgs e)
         {
+            selectedButton = color4Button; // Set selected button to color4Button
             radioButtonsGroupBox.Visibility = Visibility.Visible;
-
+            uncheckRadioButtons();
         }
+
+        private void uncheckRadioButtons()
+        {
+            redRadioButton.IsChecked = false;
+            greenRadioButton.IsChecked = false;
+            whiteRadioButton.IsChecked = false;
+            orangeRadioButton.IsChecked = false;
+            blueRadioButton.IsChecked = false;
+            yellowRadioButton.IsChecked = false;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (selectedButton != null && sender is RadioButton radioButton)
+            {
+                radioButtonsGroupBox.Visibility = Visibility.Visible;
+
+                // Set color based on selected radio button for the selected button only
+                switch (radioButton.Content.ToString())
+                {
+                    case "Rood":
+                        selectedButton.Background = Brushes.Red;
+                        break;
+                    case "Groen":
+                        selectedButton.Background = Brushes.Green;
+                        break;
+                    case "Blauw":
+                        selectedButton.Background = Brushes.Blue;
+                        break;
+                    case "Geel":
+                        selectedButton.Background = Brushes.Yellow;
+                        break;
+                    case "Wit":
+                        selectedButton.Background = Brushes.White;
+                        break;
+                    case "Oranje":
+                        selectedButton.Background = Brushes.Orange;
+                        break;
+                    default:
+                        selectedButton.Background = Brushes.Transparent;
+                        break;
+                }
+            }
+        }
+
     }
 }
