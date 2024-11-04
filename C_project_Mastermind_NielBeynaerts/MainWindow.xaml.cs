242adb919f89d5b07cecca2b2f419c8e37f88925
NielBeynaertsPXL <niel.beynaerts@student.pxl.be>
2024-11-04 15:28:50 +0100

    MasterMind-02

--- C_project_Mastermind_NielBeynaerts/MainWindow.xaml.cs
+++ C_project_Mastermind_NielBeynaerts/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -19,25 +20,47 @@ namespace C_project_Mastermind_NielBeynaerts
         public MainWindow()
         {
             InitializeComponent();
+
+            SolidColorBrush[] colors = { Brushes.Red, Brushes.Yellow, Brushes.Green, Brushes.Blue, Brushes.White, Brushes.Orange };
+            string[] colorsName = { "red", "yellow", "green", "blue", "white", "orange" };
+            int[] randomColors = new int[4];
+
+            Random rnd = new Random();
+            for (int i = 0; i < randomColors.Length; i++)
+            {
+                randomColors[i] = rnd.Next(1,6);
+            }
+            this.Title = $"{colorsName[randomColors[0]].ToString()}, {colorsName[randomColors[1]].ToString()}, {colorsName[randomColors[2]].ToString()}, {colorsName[randomColors[3]].ToString()}";
         }
+
+
+
+
+
+
+
 
         private void color1Button_Click(object sender, RoutedEventArgs e)
         {
+            radioButtonsGroupBox.Visibility = Visibility.Visible;
 
         }
 
         private void color2Button_Click(object sender, RoutedEventArgs e)
         {
+            radioButtonsGroupBox.Visibility = Visibility.Visible;
 
         }
 
         private void color3Button_Click(object sender, RoutedEventArgs e)
         {
+            radioButtonsGroupBox.Visibility = Visibility.Visible;
 
         }
 
         private void color4Button_Click(object sender, RoutedEventArgs e)
         {
+            radioButtonsGroupBox.Visibility = Visibility.Visible;
 
         }
     }
