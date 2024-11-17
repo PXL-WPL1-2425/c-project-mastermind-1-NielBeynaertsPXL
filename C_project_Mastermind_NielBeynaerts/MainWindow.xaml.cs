f9b4ac55b2890a2a640f6f4402e9def049f0c38a
NielBeynaertsPXL <niel.beynaerts@student.pxl.be>
2024-11-17 12:01:42 +0100

    MasterMind-05

--- C_project_Mastermind_NielBeynaerts/MainWindow.xaml.cs
+++ C_project_Mastermind_NielBeynaerts/MainWindow.xaml.cs
@@ -7,97 +7,155 @@ namespace C_project_Mastermind_NielBeynaerts
 {
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
         private Button selectedButton; // Track the currently selected button
+
+        string[] selectedColors = new string[4];
+        string[] randomColorSelection = new string[4];
+        int selectedColorPosition = 0;
+
+        SolidColorBrush[] chosenColors = new SolidColorBrush[4]; // Initialize array to hold 4 colors
 
         public MainWindow()
         {
             InitializeComponent();
 
-            SolidColorBrush[] colors = { Brushes.Red, Brushes.Yellow, Brushes.Green, Brushes.Blue, Brushes.White, Brushes.Orange };
-            string[] colorsName = { "Red", "Yellow", "Green", "Blue", "White", "Orange" };
-            int[] randomColors = new int[4];
-
-            Random rnd = new Random();
-            for (int i = 0; i < randomColors.Length; i++)
-            {
-                randomColors[i] = rnd.Next(0, colors.Length); // Adjusted to use 0-based index
-            }
-            this.Title = $"{colorsName[randomColors[0]]}, {colorsName[randomColors[1]]}, {colorsName[randomColors[2]]}, {colorsName[randomColors[3]]}";
+            CreateRandomColorCombination();
         }
 
+        public void CreateRandomColorCombination()
+        {
+            //SolidColorBrush[] colors = { Brushes.Red, Brushes.Yellow, Brushes.Green, Brushes.Blue, Brushes.White, Brushes.Orange };
+            string[] colorsName = { "Red", "Yellow", "Green", "Blue", "White", "Orange" };
 
+            int[] randomColors = new int[4];
+
+            for (int i = 0; i < randomColors.Length; i++)
+            {
+                randomColors[i] = rnd.Next(0, colorsName.Length); // Enters a number into array
+            }
+
+            // Display color names in the window title for debugging
+            // RandomColors[n] gives a number, this number is entered in 'ColorsName[]', this gives the name of a color and displays it as title
+            this.Title = $"{colorsName[randomColors[0]]}, {colorsName[randomColors[1]]}, {colorsName[randomColors[2]]}, {colorsName[randomColors[3]]}";
+
+            randomColorSelection[0] = colorsName[randomColors[0]];
+            randomColorSelection[1] = colorsName[randomColors[1]];
+            randomColorSelection[2] = colorsName[randomColors[2]];
+            randomColorSelection[3] = colorsName[randomColors[3]];
+
+        }
 
         private void color1Button_Click(object sender, RoutedEventArgs e)
         {
-            selectedButton = color1Button; // Set selected button to color1Button
             radioButtonsGroupBox.Visibility = Visibility.Visible;
-            uncheckRadioButtons();
+            selectedButton = color1Button;
+            selectedColorPosition = 0;
+            UncheckRadioButtons();
         }
 
         private void color2Button_Click(object sender, RoutedEventArgs e)
         {
-            selectedButton = color2Button; // Set selected button to color2Button
             radioButtonsGroupBox.Visibility = Visibility.Visible;
-            uncheckRadioButtons();
+            selectedButton = color2Button;
+            selectedColorPosition = 1;
+            UncheckRadioButtons();
         }
 
         private void color3Button_Click(object sender, RoutedEventArgs e)
         {
-            selectedButton = color3Button; // Set selected button to color3Button
             radioButtonsGroupBox.Visibility = Visibility.Visible;
-            uncheckRadioButtons();
+            selectedButton = color3Button;
+            selectedColorPosition = 2;
+            UncheckRadioButtons();
         }
 
         private void color4Button_Click(object sender, RoutedEventArgs e)
         {
-            selectedButton = color4Button; // Set selected button to color4Button
             radioButtonsGroupBox.Visibility = Visibility.Visible;
-            uncheckRadioButtons();
-        }
-
-        private void uncheckRadioButtons()
-        {
-            redRadioButton.IsChecked = false;
-            greenRadioButton.IsChecked = false;
-            whiteRadioButton.IsChecked = false;
-            orangeRadioButton.IsChecked = false;
-            blueRadioButton.IsChecked = false;
-            yellowRadioButton.IsChecked = false;
+            selectedButton = color4Button;
+            selectedColorPosition = 3;
+            UncheckRadioButtons();
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            if (selectedButton != null && sender is RadioButton radioButton)
+            if(sender is RadioButton colorRadioButton)
             {
-                radioButtonsGroupBox.Visibility = Visibility.Visible;
-
-                // Set color based on selected radio button for the selected button only
-                switch (radioButton.Content.ToString())
+                switch (colorRadioButton.Content.ToString())
                 {
                     case "Rood":
                         selectedButton.Background = Brushes.Red;
-                        break;
-                    case "Groen":
-                        selectedButton.Background = Brushes.Green;
-                        break;
-                    case "Blauw":
-                        selectedButton.Background = Brushes.Blue;
+                        selectedColors[selectedColorPosition] = "Red";
                         break;
                     case "Geel":
                         selectedButton.Background = Brushes.Yellow;
+                        selectedColors[selectedColorPosition] = "Yellow";
+                        break;
+                    case "Groen":
+                        selectedButton.Background = Brushes.Green;
+                        selectedColors[selectedColorPosition] = "Green";
+                        break;
+                    case "Blauw":
+                        selectedButton.Background = Brushes.Blue;
+                        selectedColors[selectedColorPosition] = "Blue";
                         break;
                     case "Wit":
                         selectedButton.Background = Brushes.White;
+                        selectedColors[selectedColorPosition] = "White";
                         break;
                     case "Oranje":
                         selectedButton.Background = Brushes.Orange;
+                        selectedColors[selectedColorPosition] = "Orange";
                         break;
                     default:
-                        selectedButton.Background = Brushes.Transparent;
                         break;
                 }
             }
+
         }
+
+        private void UncheckRadioButtons()
+        {
+            redRadioButton.IsChecked = false;
+            greenRadioButton.IsChecked = false;
+            blueRadioButton.IsChecked = false;
+            whiteRadioButton.IsChecked = false;
+            orangeRadioButton.IsChecked = false;
+            yellowRadioButton.IsChecked = false;
+        }
+
+        private void validateColorCode_Click(object sender, RoutedEventArgs e)
+        {
+            // Define an array of buttons corresponding to each selected color
+            Button[] buttons = { color1Button, color2Button, color3Button, color4Button };
+
+            // Loop through each selected color and its corresponding button
+            for (int i = 0; i < selectedColors.Length; i++)
+            {
+                // Check for an exact match
+                if (selectedColors[i] == randomColorSelection[i])
+                {
+                    SetButtonStyle(buttons[i], new Thickness(2, 2, 2, 20), Colors.DarkRed);
+                }
+                // Check for a partial match
+                else if (randomColorSelection.Contains(selectedColors[i]))
+                {
+                    SetButtonStyle(buttons[i], new Thickness(2, 2, 2, 20), Colors.Wheat);
+                }
+            }
+        }
+
+        private void SetButtonStyle(Button button, Thickness thickness, Color color)
+        {
+            button.BorderThickness = thickness;
+            button.BorderBrush = new SolidColorBrush(color);
+        }
+
+
+
+
+
 
     }
 }
